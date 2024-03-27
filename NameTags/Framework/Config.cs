a9656ea17630621a9b91532f4028479ddc33cd9e
Enaium <wyy-666@hotmail.com>
2024-03-27 23:15:43 +0800

    update to 1.6

---
+++ NameTags/Framework/Config.cs
@@ -0,0 +1,16 @@
+﻿using EnaiumToolKit.Framework.Utils;
+using StardewModdingAPI;
+
+namespace NameTags.Framework;
+
+public class Config
+{
+    public SButton OpenSetting { get; set; } = SButton.N;
+    public ColorUtils.NameType TextColor = ColorUtils.NameType.White;
+    public bool RenderMonster { get; set; } = true;
+    public bool RenderPet { get; set; } = true;
+    public bool RenderHorse { get; set; } = true;
+    public bool RenderChild { get; set; } = true;
+    public bool RenderVillager { get; set; } = true;
+    public bool RenderJunimo { get; set; } = true;
+}
